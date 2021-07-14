ae43acb3603eb62cde2a66cb272916a94b000119
Franck Duval <10franckpertinent@gmail.com>
2021-07-14 20:50:19 +0100

    Initialization of this project

---
+++ pages/_app.js
@@ -0,0 +1,43 @@
+import Root from "./routes/@root";
+import App from "next/app";
+import { Fragment, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Axios from 'axios';
+
+Axios.defaults.baseURL = "";
+Axios.defaults.withCredentials = true;
+
+function MyApp( { Component, pageProps } ) {
+    const 
+        route = useRouter(),
+        event = 'routeChangeComplete',
+        handle = () => (
+            window.location.reload()
+        );
+            useEffect( function () {
+                route.events.on( event, handle );
+                return function () {
+                    return route.events.off( event, handle );
+                };
+            } );
+    return (
+        <Fragment>
+            <Root page={ Component.page }>
+                <Component { ...pageProps } />
+            </Root>
+            { Component.scripts ? Component.scripts.map( ( url ) => (
+                <script src={ url } key={ url }></script>
+            ) ) : "" }
+        </Fragment>
+    );
+};
+
+MyApp.getInitialProps = async ( appContext ) => {
+    const
+        appProps = await App.getInitialProps( appContext );
+    return {
+        ...appProps
+    };
+};
+
+export default MyApp;
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { Fragment } from 'react';
+
+export const page = 'home';
+export default function Home () {
+    return (
+        <Fragment>
+            <Head>
+                
+            </Head>
+            <div> index </div>
+        </Fragment>
+    );
+};
