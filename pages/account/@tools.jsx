4e999b738e780c4d8acddf4d6e580e7b0d81a5c3
Franck Duval <10franckpertinent@gmail.com>
2021-08-11 07:11:34 +0100

    Final Version

--- pages/account/@tools.jsx
+++ pages/account/@tools.jsx
@@ -5,11 +5,10 @@ import Sticky from './tools/@sticky-note';
 export function ToolItem( { name, size, icon, onClick } ) {
     const 
         [ display, setDisplay ] = useState( 'hide-i' ),
-        hover = function () {
-            setDisplay( display === 'show-i' ? 'hide-i' : 'show-i' );
-        };
+        enter = () => setDisplay( 'show-i' ),
+        out = () => setDisplay( 'hide-i' );
     return (
-        <div className="d-flex flex-row tool-item justify-content-center align-items-center mb-3" onClick={ onClick } onMouseEnter={ hover } onMouseLeave={ hover }>
+        <div className="d-flex flex-row tool-item justify-content-center align-items-center mb-3" onClick={ onClick } onMouseEnter={ enter } onMouseLeave={ out } onMouseUp={ out }>
             <div className="tool-item-icon d-flex justify-content-center align-items-center" style={ { width: size, height: size } }>
                 <i className={ "d-block bi bi-".concat( icon ) } style={ { fontSize: size * 0.45 } }></i>
             </div>
