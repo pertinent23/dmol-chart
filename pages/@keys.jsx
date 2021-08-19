174795f00083def24782839fc0ca6e25369bf7d9
Franck Duval <10franckpertinent@gmail.com>
2021-08-19 19:07:01 +0100

    new version

--- pages/@keys.jsx
+++ pages/@keys.jsx
@@ -4,6 +4,6 @@ export default Root;
 export const keys = {
     TWITTER_SECRET: "",
     TWITTER_PUPLIC: "",
-    FACEBOOK: "522395098976902",
+    FACEBOOK: "896731337930498",
     GOOGLE: "584173251969-2mhb55ak9nrh5bseb58paf4g7jv0js10.apps.googleusercontent.com"
 };
